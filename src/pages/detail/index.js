d635c1f534efd6787cd67c1b13a1d0f07f753a09
HWfuture <18320398437@139.com>
2020-11-07 10:59:26 +0800

    add detail react-redux

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { DetailWrapper, Header, Content } from './style'
 
 class Detail extends Component {
@@ -6,30 +7,18 @@ class Detail extends Component {
         return (
             <div>
                 <DetailWrapper>
-                    <Header>请记住杨绛先生的这4句话（深度文字）</Header>
-                    <Content>
-                        <p>人的一生是一场漫长而坎坷的旅程，多去汲取别人人生的经验，我们在路途上就能少走些弯路，受益终生。</p>
-                        <p>杨绛先生的这4句话，字字都是珠玑，读懂了它们，就读懂了人生。</p>
-                        <img src="https://upload-images.jianshu.io/upload_images/10472974-184bbe1cd4d6ea1b.jpeg?imageMogr2/auto-orient/strip|imageView2/2/w/700/format/webp" alt=""></img>
-                                <h3>你的问题主要在于读书不多而想得太多</h3>
-                                <p>人为什么要读书？</p>
-                                <p>许多人也许都曾问过自己这样一个问题？</p> 
-                                <p>“我读过的书，很快就都忘记了，那么读书究竟还有什么用呢？”</p>
-                                <p> 三毛说：</p>
-                                <p>“读书多了，容颜自然改变，许多时候，自己可能以为许多看过的书籍都成过眼烟云，不复记忆，其实它们仍是潜在气质里、在谈吐上、在胸襟的无涯，当然也可能显露在生活和文字中。”</p>
-                                <p>你读过的书，最终决定了你是什么样的人。</p>
-                                <p>杨绛先生从小就爱读书，在一篇《我爱清华图书馆》的文章里，她温情地把读书比作“串门儿”：</p>
-                                <p>“借书看，只是要求到某某家去‘串门儿’，而站在图书馆书库的书架前任意翻阅，就好比家家户户都可任意出入，这是唯有身经者才知道的乐趣。”</p>
-                                <p> 不读书的人，只过一世；读书的人，却可以体验千百种人生。</p>
-                                <p> 在人生的目标还没有清晰显现之前，与其焦灼、忧虑，不如去读一本好书，静下心来充实自己。</p>
-                                <p> 你要相信，你若盛开，清风自来。</p>
-                                <p> 当你真正准备好的时候，整个世界都会为你让步。</p>
-                        <img src="https://upload-images.jianshu.io/upload_images/10472974-9fe255ead2cf4551.jpeg?imageMogr2/auto-orient/strip|imageView2/2/w/700/format/webp"  alt="" />
-                    </Content>
+                    <Header>{ this.props.title}</Header>
+                    <Content dangerouslySetInnerHTML={{  __html: this.props.content }}  />  
+                    {/* dangerouslySetInnerHTML 双{{}} 包裹 */}
                 </DetailWrapper>
             </div>
         )
     }
 }
 
-export default Detail
+const mapState = (state) => ({
+    title: state.getIn(['detail', 'title']),
+    content: state.getIn(['detail', 'content'])
+})
+
+export default connect(mapState, null) (Detail)
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux-immutable' // 统一为immutable的 数据格式
 import { reducer as headerReducer } from '../common/header/store'
 import { reducer as homeReducer } from '../pages/home/store'
+import { reducer as detailReducer } from '../pages/detail/store'
 
 // combineReducers 可以结合多个reducer 方便管理。
 // redux-immutable 中也有 combineReducers 转化为immutable对象
 const reducer = combineReducers({
     header: headerReducer,
-    home: homeReducer
+    home: homeReducer,
+    detail: detailReducer
 })
 
 export default reducer
